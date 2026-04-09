2274e1e1a7916e22b69c177635d305472eeda043
Emilie <emiliedumont29@gmail.com>
2026-04-09 15:03:29 +0200

    structure MVC initiale

---
+++ app/config/params_example.php
@@ -0,0 +1,9 @@
+<?php
+//1.initialiser les zones dynamiques :$content
+$content = 'Aucun contenu ne correspond à votre requête';
+
+//2.Initialiser les constantes de connexion (DB_HOST, DB_NAME, DB_USER, DB_PASSWORD)
+define('DB_HOST', 'your_host');
+define('DB_NAME', 'your_db_name');
+define('DB_USER', 'your_db_user');
+define('DB_PASSWORD', 'your_db_password');
